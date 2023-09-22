595b94d9610c87fe943935f95ba431b2e924244b
Geoffrey Herney <gherney@gmail.com>
2023-09-22 15:58:38 -0400

    chore: [CAGG-5117] honor url blacklist when logging connection errors (#2)

--- spec/lib/http_log_spec.rb
+++ spec/lib/http_log_spec.rb
@@ -416,9 +416,18 @@ describe HttpLog do
             allow_any_instance_of(adapter_class.native_adapter_class).to receive(:orig_connect).and_raise(Timeout::Error.new)
             expect{adapter.send_connect}.to raise_error(Timeout::Error)
           end
-          it { expect(json['response_headers']['error']).to eq '#<Timeout::Error: Timeout::Error>' }
-          it { expect(json['benchmark']).to be > 0 }
-          it { expect(json['url']).to eq "http://#{host}:#{port}" }
+
+          context 'with blacklist miss' do
+            let(:url_blacklist_pattern) { /example.com/ }
+            it { expect(json['response_headers']['error']).to eq '#<Timeout::Error: Timeout::Error>' }
+            it { expect(json['benchmark']).to be > 0 }
+            it { expect(json['url']).to eq "http://#{host}:#{port}" }
+          end
+
+          context 'with blacklist hit' do
+            let(:url_blacklist_pattern) { /#{host}:#{port}/ }
+            it_behaves_like 'logs nothing'
+          end
         end
       end
 
@@ -429,10 +438,19 @@ describe HttpLog do
             allow_any_instance_of(adapter_class.native_adapter_class).to receive(:orig_request).and_raise(IOError.new('closed stream'))
             expect{adapter.request_error}.to raise_error(IOError)
           end
-          it { expect(json['method']).to eq 'GET' }
-          it { expect(json['url']).to eq "http://#{host}:#{port}#{path}?#{data}" }
-          it { expect(json['response_headers']['error']).to eq '#<IOError: closed stream>' }
-          it { expect(json['benchmark']).to be > 0 }
+
+          context 'with blacklist miss' do
+            let(:url_blacklist_pattern) { /example.com/ }
+            it { expect(json['method']).to eq 'GET' }
+            it { expect(json['url']).to eq "http://#{host}:#{port}#{path}?#{data}" }
+            it { expect(json['response_headers']['error']).to eq '#<IOError: closed stream>' }
+            it { expect(json['benchmark']).to be > 0 }
+          end
+
+          context 'with blacklist hit' do
+            let(:url_blacklist_pattern) { /#{host}:#{port}/ }
+            it_behaves_like 'logs nothing'
+          end
         end
       end
     end
